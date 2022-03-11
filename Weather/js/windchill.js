c53d7c7a47df59ff552c56c68954fd410ab33d30
PrehistoricTree67 <jli007@byui.edu>
2022-03-11 10:05:34 -0500

    commit

--- Weather/js/windchill.js
+++ Weather/js/windchill.js
@@ -1,9 +1,12 @@
-function inPutoutPut {
-    let a = parseInt(document.getElementById('currently').value);
-    
+function inPutoutPut() {
+    let a = parseInt(document.getElementById('wcf').value);
+    let y = parseInt(document.getElementById('high').value);
+    let g = parseInt(document.getElementById('humidity').value);
+    let results = windChill(a, y, g);
+    document.getElementById('wind-speed').innerHTML = results;
 }
 
-function windchill(tempFahren, mph){
+function windChill(tempFahren, mph){
     let t = tempFahren;
     let s = mph;
     let f = 35.74 + 0.6215 * t - 35.75 * s ** 0.16 + 0.4275 * t * s ** 0.16;
