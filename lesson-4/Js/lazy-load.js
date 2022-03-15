ee599b2cea12584b730ba0002e26a8dc4ac110a0
PrehistoricTree67 <jli007@byui.edu>
2022-03-15 19:38:17 -0400

    commit

---
+++ lesson-4/Js/lazy-load.js
@@ -0,0 +1,31 @@
+const img = document.querySelectorAll("[src]");
+
+function preloadImage(img) {
+    const src = img.getAttribute("src");
+    if(!src) {
+        return;
+    }
+
+    img.src=src;
+}
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 200px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries,
+imgObserver) => {
+entries.forEach(entry => {
+    if(!entry.isIntersecting) {
+        return;
+    } else {
+        preloadImage(entry.target);
+        imgObserver.unobserve(entry.target);
+    }
+})
+}, imgOptions);
+
+images.forEach(image => {
+    imgObserver.observe(image);
+});
