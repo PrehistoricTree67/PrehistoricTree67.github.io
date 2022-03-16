21ac825430e8979920caa59672abc162145cfb94
PrehistoricTree67 <jli007@byui.edu>
2022-03-16 10:56:27 -0400

    commit

--- Weather/js/preston.js
+++ Weather/js/preston.js
@@ -10,3 +10,7 @@ document.querySelectorAll(".act").forEach(n => n.addEventListener("click", () =>
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+if(new Date().getDay()!= 3) {
+    document.getElementsByClassName("q").style.display = "none";
+}
