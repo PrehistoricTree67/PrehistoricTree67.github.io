d9d25f540149e42667c3b8256419ecf9a6b7a373
PrehistoricTree67 <jli007@byui.edu>
2022-03-21 14:59:16 -0400

    commit

--- Weather/js/preston.js
+++ Weather/js/preston.js
@@ -12,3 +12,13 @@ mainnav.classList.remove('responsive')};
 if(new Date().getDay()!= 6) {
     document.getElementById("days").style.display = "none";
 }
+
+
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+fetch(requestURL)
+    .then(function (response){
+       return response.json();
+    })
+    .then(function (jsonObject){
+        console.table(jsonObject); const towns = jsonObject['towns'];
+    })
