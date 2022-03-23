a9a8186deb0d0fa8169741ddff9565f278580c9d
PrehistoricTree67 <jli007@byui.edu>
2022-03-23 14:08:58 -0400

    commit

--- lesson-10/js/weatherapi.js
+++ lesson-10/js/weatherapi.js
@@ -2,5 +2,7 @@ const apiURL = "https://api.openweathermap.org/...";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
+    console.log(jsObject); {
+        document.getElementById('current-temp').textContent = jsObject.main.temp;
+    }
   });
