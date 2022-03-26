b7317193892751dd85515af0cc41a352b9e726cf
PrehistoricTree67 <jli007@byui.edu>
2022-03-26 19:00:55 -0400

    commit

--- Weather/js/windchill.js
+++ Weather/js/windchill.js
@@ -13,16 +13,3 @@ function windChill(tempFahren, mph){
     let f = 35.74 + 0.6215 * t - 35.75 * s ** 0.16 + 0.4275 * t * s ** 0.16;
     return f;
 }
-
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Preston,5604473&appid=d8504fb6db3692f9637d82ee139508e9&units=imperial";
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject); 
-        document.getElementById('current-temp').textContent = jsObject.main.temp;
-        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-        const desc = jsObject.weather[0].description;  // note how we reference the weather array
-        document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-        document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-        document.getElementById('icon').setAttribute('alt', desc);
-  });
