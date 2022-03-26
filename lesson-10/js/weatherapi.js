72b6714d1a530acd84e551c1ebc7307fc332b0d1
PrehistoricTree67 <jli007@byui.edu>
2022-03-26 17:31:04 -0400

    commit

--- lesson-10/js/weatherapi.js
+++ lesson-10/js/weatherapi.js
@@ -1,13 +1,12 @@
-const apiURL = "https://api.openweathermap.org/...";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Preston,5604473&appid=d8504fb6db3692f9637d82ee139508e9&units=imperial";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject); {
+    console.log(jsObject); 
         document.getElementById('current-temp').textContent = jsObject.main.temp;
-    }
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+        const desc = jsObject.weather[0].description;  // note how we reference the weather array
+        document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+        document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+        document.getElementById('icon').setAttribute('alt', desc);
   });
-  const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-  const desc = jsObject.weather[0].description;  // note how we reference the weather array
-  document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-  document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-  document.getElementById('icon').setAttribute('alt', desc);
