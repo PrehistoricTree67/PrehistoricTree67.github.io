b5e4d2feed6e3bc08c7944c8ef0ce0c804c6f5fe
PrehistoricTree67 <jli007@byui.edu>
2022-03-28 21:29:21 -0400

    commit

--- Weather/js/preston.js
+++ Weather/js/preston.js
@@ -62,3 +62,38 @@ fetch(requestURL)
                 return jsonObject;
             }
         });
+
+
+const weathapi = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=e393808b164a35213aa10cb637f93d76&units=imperial';
+  const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  fetch(weathapi)
+  .then(function (response) {
+    return response.json();
+})
+.then(function (jsonObject) {
+    console.table(jsonObject); 
+    let list = jsonObject.list;
+    console.log(list);
+    let newList = [];
+    for(let i = 0; i < list.length; i++){
+        if(list[i].dt_txt.includes("18:00:00")){
+            newList.push(list[i]);
+        }
+    }
+    console.log(newList);
+    const day1 = new Date(newList[0].dt_txt);
+    document.getElementById('day1').innerHTML = days[day1.getDay()];
+    const day2 = new Date(newList[1].dt_txt);
+    document.getElementById('day2').innerHTML = days[day2.getDay()];
+    const day3 = new Date(newList[2].dt_txt);
+    document.getElementById('day3').innerHTML = days[day3.getDay()];
+    const day4 = new Date(newList[3].dt_txt);
+    document.getElementById('day4').innerHTML = days[day4.getDay()];
+    const day5 = new Date(newList[4].dt_txt);
+    document.getElementById('day5').innerHTML = days[day5.getDay()];
+    document.getElementById('climate1').innerHTML=newList[0].main.temp;
+    document.getElementById('climate2').innerHTML=newList[1].main.temp;
+    document.getElementById('climate3').innerHTML=newList[2].main.temp;
+    document.getElementById('climate4').innerHTML=newList[3].main.temp;
+    document.getElementById('climate5').innerHTML=newList[4].main.temp;
+});
