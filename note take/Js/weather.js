578a1359f4fd1b46fa03ebc6ca83dda204ba997a
PrehistoricTree67 <jli007@byui.edu>
2022-04-06 01:43:08 -0400

    commit

--- note take/Js/weather.js
+++ note take/Js/weather.js
@@ -1,5 +1,5 @@
 const weather = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=e393808b164a35213aa10cb637f93d76';
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const days = ["Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday", "Sunday"]
 fetch(weather)
     .then(function (response){
         return response.json();
@@ -14,4 +14,4 @@ fetch(weather)
                 newList.push(list[r]);
             }
         }
-    })
+    });
