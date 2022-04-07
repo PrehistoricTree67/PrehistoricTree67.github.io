4fead6cc7fb7af75242ade6882e5dfd5d59e30f1
PrehistoricTree67 <jli007@byui.edu>
2022-04-06 22:53:30 -0400

    commit

--- lesson-10/js/rent.js
+++ lesson-10/js/rent.js
@@ -1,1 +1,9 @@
-const cars = '/'
+const cars = 'file:///Users/jonahli/Desktop/PrehistoricTree67.github.io/lesson-10/js/rental.json';
+fetch(cars)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (jsonObject){
+        console.table(jsonObject);
+
+    })
