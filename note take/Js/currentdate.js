fb6add8868c2841b81fec5db821f4e060e3b2b59
PrehistoricTree67 <jli007@byui.edu>
2022-04-08 00:21:08 -0400

    commit

--- note take/Js/currentdate.js
+++ note take/Js/currentdate.js
@@ -1,2 +1,35 @@
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById("currentdate").innerHTML = new Date().toLocaleDateString('en-US', options);
+//day names array
+const daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thrusday",
+    "Friday",
+    "Saturday"
+];
+
+
+//long month names array
+const months = [
+    "Januray",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+const todaysdate = new Date();
+const dayName = daynames[todaysdate.getDay()];
+const monthName = months[todaysdate.getMonth()];
+const year = todaysdate.getFullYear;
+const currentdate = dayName + ", " + todaysdate.getDate() + " " + monthName + ", " + todaysdate.getFullYear();
+
+document.getElementById("currentdate").textContent = currentdate
